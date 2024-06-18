204b60f08418ccba82f0755bb5096d2da049b1b6
BaifernCho <fern9222@gmail.com>
2024-06-18 19:30:02 +0700

    Create cal_grade.py

---
+++ cal_grade.py
@@ -0,0 +1,21 @@
+
+print(f"การคำนวณเกรด 5วิชา")
+
+
+subject_all = ["THAI", "MATH", "ENGLISH", "SCIENCE", "SPORT"]
+grade_all =[]
+
+for subject in subject_all:
+  grade = float(input())
+  grade_all.append(grade)
+
+for subject, grade in zip (subject_all, grade_all):
+    print(f"{subject} = {grade}")
+
+print("---")
+
+gpa = sum(grade_all)/len(grade_all)
+
+print(f"GPA = {gpa}")
+
+
