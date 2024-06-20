27cc649436770f215714cbb5a9610161148c4157
BaifernCho <fern9222@gmail.com>
2024-06-20 17:06:35 +0700

    Create LV.1_grade.py

---
+++ LV.1_grade.py
@@ -0,0 +1,27 @@
+score = float(input())
+
+if score >= 90:
+  print(f"A")
+  
+elif score >= 85:
+  print(f"B+")
+
+elif score >= 80:
+  print(f"B")
+
+elif score >= 75:
+  print(f"C+")
+
+elif score >= 70:
+  print(f"C")
+
+elif score >= 65:
+  print(f"D+")
+
+elif score >= 60:
+  print(f"D")
+
+else:
+  print(f"F")
+  
+  
